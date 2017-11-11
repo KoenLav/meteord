3da5596c51d0615e4585ed714723a26e82dc6795
KoenLav <k.lavrijssen@gmail.com>
2017-11-11 16:25:53 +0100

    Remove tests again

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,7 @@ export NODE_VERSION=${NODE_VERSION:-4.8.6}
 bash ./build_it.sh
 
 bash ./test_meteor_app.sh
-bash ./test_meteor_app_with_devbuild.sh
+#bash ./test_meteor_app_with_devbuild.sh
 
 # I don't believe it's possible for this test to work right now.
 # bash ./test_bundle_local_mount.sh
@@ -17,5 +17,5 @@ bash ./test_meteor_app_with_devbuild.sh
 #bash ./test_binary_build_on_base.sh
 #bash ./test_binary_build_on_bin_build.sh
 
-bash ./test_phantomjs.sh
-bash ./test_no_app.sh
+#bash ./test_phantomjs.sh
+#bash ./test_no_app.sh

ea01029711ab2e9a30b42f0ef4e84f7be83cc1e7
KoenLav <k.lavrijssen@gmail.com>
2017-11-11 15:28:18 +0100

    Disable tests again

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -5,8 +5,8 @@ set -x
 
 docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
   docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
+#docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  #docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
 #docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
 #  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
 #docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \

98adaad05c16c06841e26e1e4135ba11f7b86bc6
KoenLav <k.lavrijssen@gmail.com>
2017-11-11 15:03:43 +0100

    Make own build

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -7,7 +7,7 @@ docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node
   docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
 docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
   docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+#docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
+#  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
+#docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
+#  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
